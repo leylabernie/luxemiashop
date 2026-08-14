d700cfe9bec48dd80175a8ced4125992804be91a
leylabernie <bhamini@verizon.net>
2026-08-14 09:58:29 -0400

    Add recovered collection landing page component

---
+++ src/pages/RecoveryCollection.tsx
@@ -0,0 +1,15 @@
+import CategoryListing from '@/components/collections/CategoryListing';
+import {
+  getRecoveryCollectionConfig,
+  type RecoveryCollectionHandle,
+} from '@/config/recoveryCollectionConfig';
+
+interface RecoveryCollectionProps {
+  handle: RecoveryCollectionHandle;
+}
+
+const RecoveryCollection = ({ handle }: RecoveryCollectionProps) => (
+  <CategoryListing config={getRecoveryCollectionConfig(handle)} />
+);
+
+export default RecoveryCollection;
