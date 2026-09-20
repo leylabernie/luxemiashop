e7ad8986a8c16382f37f8f979a75e08bd40d1d14
leylabernie <leylabernie@users.noreply.github.com>
2026-09-19 21:24:10 -0400

    Correct per-slide banner anchors: Luxe Festive left, Bridal Elegance right

--- src/components/home/NewArrivalsBanner.tsx
+++ src/components/home/NewArrivalsBanner.tsx
@@ -47,7 +47,7 @@ const featuredSlides: FeaturedSlide[] = [
     width: 1920,
     height: 800,
     imageFit: 'cover',
-    imagePosition: 'right center',
+    imagePosition: 'left center',
     designed: true,
   },
   {
@@ -97,7 +97,7 @@ const featuredSlides: FeaturedSlide[] = [
     width: 1920,
     height: 800,
     imageFit: 'cover',
-    imagePosition: 'left center',
+    imagePosition: 'right center',
     designed: true,
   },
 ];
